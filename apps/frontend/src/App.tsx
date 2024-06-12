c310cbed40a3582919cfbbc52a3fc5a6b62132c7
milindrai <milindrai365@gmail.com>
2024-06-12 19:54:38 +0530

    Final commit

---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,43 @@
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Landing } from './screens/Landing';
+import { Game } from './screens/Game';
+import Login from './screens/Login';
+import { Suspense } from 'react';
+import { RecoilRoot } from 'recoil';
+import { useUser } from '@repo/store/useUser';
+import { Loader } from './components/Loader';
+import { Layout } from './layout';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-stone-800">
+      <RecoilRoot>
+        <Suspense fallback={<Loader />}>
+          <AuthApp />
+        </Suspense>
+      </RecoilRoot>
+    </div>
+  );
+}
+
+function AuthApp() {
+  const user = useUser();
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout children={<Landing />} />} />
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+        <Route
+          path="/game/:gameId"
+          element={<Layout children={<Game />} />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
